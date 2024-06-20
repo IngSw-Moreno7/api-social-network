4ece633a151bca9e32d1ebe644957a86241c89d4
Paola Andrea Moreno <136043704+IngSw-Moreno7@users.noreply.github.com>
2024-06-20 11:14:39 -0500

    test-route OK http://localhost:3900/test-route

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-//Importaciones
+// Importaciones
+const connection = require("./database/connection");
+const express = require("express");
+const cors = require("cors");
 
-//Creación a la conexión a la BD
+// Mensaje de bienvenida
+console.log("ARI NODE arriba");
 
-//Configurar cors: permite que las peticiones se hagan correctamente
+// Creación a la conexión a la BD
+connection();
+
+// Crear servidor de Node
+const app = express();
+const puerto = 3900;
+
+// Configurar cors: permite que las peticiones se hagan correctamente
+app.use(cors());
 
 // Conversión de datos (body a objetos JS)
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-//Configuración de rutas
+// Configuración de rutas
+app.get('/test-route', (req, res) => {
+  return res.status(200).json(
+    {
+      'id': 1,
+      'name': 'Paola Andrea Moreno',
+      'username': 'paolamore'
+    }
+  );
+})
 
-//Configurar el servidor para escuchar las peticiones HTTP
+
+// Configurar el servidor para escuchar las peticiones HTTP
+app.listen(puerto, () => {
+  console.log("Servidor de NODE corriendo en el puerto", puerto)
+});
