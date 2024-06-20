f0cbc8c913c4703e81ef1e04523201c4f99cc824
Paola Andrea Moreno <136043704+IngSw-Moreno7@users.noreply.github.com>
2024-06-20 14:36:46 -0500

    Creating models folder with user.js file, OK

---
+++ models/user.js
@@ -0,0 +1,38 @@
+import {schema, model, Schema} from "mongoose";
+
+const UserSchema = Schema ({
+  name: {
+    type: String,
+    required: true
+  },
+  last_name: {
+    type: String,
+    required: true
+  },
+  nick: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    required: "role_user"
+  },
+  image: {
+    type: String,
+    required: "default.png"
+  },
+  created_at: {
+    type: Date,
+    required: Date.now
+  }
+});
+
+export default model("User", UserSchema, "users");
