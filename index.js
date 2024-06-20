c68a1c03cdd7b76f0d7ce8cbe59914df056c357a
Paola Andrea Moreno <136043704+IngSw-Moreno7@users.noreply.github.com>
2024-06-20 00:14:00 -0500

    Initial project and start-project.bat

---
+++ index.js
@@ -0,0 +1,11 @@
+//Importaciones
+
+//Creación a la conexión a la BD
+
+//Configurar cors: permite que las peticiones se hagan correctamente
+
+// Conversión de datos (body a objetos JS)
+
+//Configuración de rutas
+
+//Configurar el servidor para escuchar las peticiones HTTP
---
+++ start-project.bat
@@ -0,0 +1,2 @@
+start cmd.exe /k "cd /d C:\Program Files\MongoDB\Server\7.0\bin && mongod.exe --dbpath C:\Users\paola\ProjectsBootcamp\bdata-mongodb"
+start cmd.exe /k "cd /d C:\Users\paola\ProjectsBootcamp\api-social-network && npm run start:watch"
