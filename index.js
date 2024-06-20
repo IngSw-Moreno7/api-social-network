506d24d707aa18f2545df8b3bd49da13feddc86a
Paola Andrea Moreno <136043704+IngSw-Moreno7@users.noreply.github.com>
2024-06-20 14:07:28 -0500

    test routes, working OK

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 import connection from "./database/connection.js";
 import express, { json, urlencoded } from "express";
 import cors from "cors";
+import UserRoutes from "./routes/user.js";
+import PublicationsRoutes from "./routes/publications.js";
+import FollowRoutes from "./routes/follow.js";
 
 // Mensaje de bienvenida
 console.log("API NODE arriba");
@@ -21,6 +24,10 @@ app.use(json());
 app.use(urlencoded({extended: true}));
 
 // Configuración de rutas
+app.use('/api/user', UserRoutes);
+app.use('/api/publications', PublicationsRoutes);
+app.use('/api/follow', FollowRoutes);
+
 app.get('/test-route', (req, res) => {
   return res.status(200).json(
     {
@@ -30,7 +37,6 @@ app.get('/test-route', (req, res) => {
     }
   );
 })
-
 
 // Configurar el servidor para escuchar las peticiones HTTP
 app.listen(puerto, () => {
