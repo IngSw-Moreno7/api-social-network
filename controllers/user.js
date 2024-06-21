44e9d9807803b54ce929bded3be97836c90f88c3
Paola Andrea Moreno <136043704+IngSw-Moreno7@users.noreply.github.com>
2024-06-21 01:04:03 -0500

    Added method to authenticate users in controllers/user.js

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
+import { createToken } from "../services/jwt.js";
 
 // Acciones de prueba
 export const testUser = (req, res) => {
@@ -21,10 +22,11 @@ export const register = async (req, res) => {
         message: "Faltan datos por enviar"
       });
     }
+
     // Crear una instancia del modelo User con los datos validados
     let user_to_save = new User(params);
 
-   // Buscar si ya existe un usuario con el mismo email o nick
+    // Buscar si ya existe un usuario con el mismo email o nick
     const existingUser = await User.findOne({
       $or: [
         { email: user_to_save.email.toLowerCase() },
@@ -39,6 +41,7 @@ export const register = async (req, res) => {
         message: "!El usuario ya existe!"
       });
     }
+
     // Cifrar contraseña
     const salt = await bcrypt.genSalt(10);
     const hasedPassword = await bcrypt.hash(user_to_save.password, salt);
@@ -59,6 +62,73 @@ export const register = async (req, res) => {
     return res.status(500).json({
       status: "error",
       message: "Error en registro de usuarios"
+    });
+  }
+}
+
+// Método para autenticar usuarios
+export const login = async (req, res) => {
+  try {
+
+    // Recoger los parámetros del body
+    let params = req.body;
+
+    // Validar si llegaron el email y password
+    if (!params.email || !params.password){
+      return res.status(400).send({
+        status: "error",
+        message: "Faltan datos por enviar"
+      });
+    }
+
+    // Buscar en la BD si existe el email que nos envió el usuario
+    const user = await User.findOne({ email: params.email.toLowerCase()});
+
+    // Si no existe el user
+    if (!user) {
+      return res.status(404).json({
+        status: "error",
+        message: "Usuario no encontrado"
+      });
+    }
+
+    // Comprobar si el password recibido es igual al que está almacenado en la BD
+    const validPassword = await bcrypt.compare(params.password, user.password);
+
+    // Si los passwords no coinciden
+    if (!validPassword) {
+      return res.status(401).json({
+        status: "error",
+        message: "Contraseña incorrecta"
+      });
+    }
+
+    // Generar token de autenticación
+    const token = createToken(user);
+
+    // Devolver Token generado y los datos del usuario
+    return res.status(200).json({
+      status: "success",
+      message: "Login exitoso",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        last_name: user.last_name,
+        bio: user.bio,
+        email: user.email,
+        nick: user.nick,
+        role: user.role,
+        image: user.image,
+        created_at: user.created_at
+      }
+    });
+
+  } catch (error) {
+    console.log("Error en el login del usuario: ", error);
+    return res.status(500).send({
+      status: "error",
+      message: "Error en el login del usuario"
     });
   }
 }
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
 //Importaciones
 import { Router } from "express";
 const router = Router();
-import { register, testUser } from "../controllers/user.js";
+import { register, testUser, login } from "../controllers/user.js";
 
 //Definir las rutas
 router.get('/test-user', testUser);
 router.post('/register', register);
+router.post('/login', login);
 
 //Exportar el router
 export default router;
